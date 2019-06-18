8e23db29d7b6f39ebd399b2d6fd632569b30de90
Liviu Sultan | eMAG <liviu.sultan@emag.ro>
2019-06-18 12:09:40 +0300

    change product exception and create new Exception class

---
+++ src/CourierBundle/DeliveryValidator/Exception/ProductNotSupportedException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liviu.sultan
+ * Date: 18-Jun-19
+ * Time: 12:00 PM
+ */
+
+namespace CourierBundle\DeliveryValidator\Exception;
+
+
+class ProductNotSupportedException extends \Exception
+{
+
+}
